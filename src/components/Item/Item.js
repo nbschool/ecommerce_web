039bbd7af5c28be44cce357cd1623b549a4b8d90
Alessandro Brugioni <info@toysroom.it>
2017-05-03 20:23:19 +0200

    added Item and ItemList component

---
+++ src/components/Item/Item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Item.css';
+
+const Item = ({item}) => {
+  return (
+    <article key={item.item_id} className="Item">
+      <div className="card">
+        <div className="image">
+          <img src="http://placehold.it/150x250" />
+        </div>
+        <div className="info">
+          <div className="name">{item.name}</div>
+          <div className="price">€{item.price}</div>
+          <div className="description">{item.description}</div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+Item.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+export default Item;
