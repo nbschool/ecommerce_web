d7dfdced6034e65843ed8b60ba3b2e72b9cf7a24
Alessandro Brugioni <info@toysroom.it>
2017-05-10 12:15:16 +0200

    removed unused html nav menu and react link of react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Link } from 'react-router-dom';
-
 import './App.css';
 
 const App = (props) => (
   <div>
-    <ul>
-      <li><Link to="/">ItemList</Link></li>
-    </ul>
-    <hr/>
     {props.children}
   </div>
 );
