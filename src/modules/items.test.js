0665c04d06f979c336734327973c736931c09a5a
Francesco Mirabelli <ceskomira90@gmail.com>
2017-05-19 09:34:33 +0200

    imported Promise to avoid error unused import

--- src/modules/items.test.js
+++ src/modules/items.test.js
@@ -1,6 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock from 'fetch-mock';
+import Promise from 'Promise';
 
 import reducer, {
   fetchItems,
