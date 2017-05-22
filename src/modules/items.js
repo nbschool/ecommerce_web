7ac66eaa45ba4914dc0b46d3b8eeb7676d931cbe
Francesco Mirabelli <ceskomira90@gmail.com>
2017-05-22 11:48:55 +0200

    removed useless loaded in FETCH_PICTURE

--- src/modules/items.js
+++ src/modules/items.js
@@ -131,7 +131,6 @@ export default function reducer(state = initialState, action = {}) {
     return {
       ...state,
       items: addPictureToItem(action.payload, state.items),
-      loaded: true,
     };
 
   default:
