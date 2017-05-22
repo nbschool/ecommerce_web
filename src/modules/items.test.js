429f6f11d8064d7974e63e3368ce77139ca7fa37
Francesco Mirabelli <ceskomira90@gmail.com>
2017-05-22 10:05:17 +0200

    deleted unusued import and add configuration for Promise  to eslint

--- src/modules/items.test.js
+++ src/modules/items.test.js
@@ -1,7 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock from 'fetch-mock';
-import Promise from 'Promise';
 
 import reducer, {
   fetchItems,
