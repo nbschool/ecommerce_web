6c07e507cebee8f840dd3834229d413752d87e12
Alessandro Brugioni <info@toysroom.it>
2017-05-24 13:07:54 +0200

    add file with translations strings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { I18nextProvider } from 'react-i18next';
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -10,6 +11,8 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import rootReducer from './modules';
 import App from './components/App';
+import i18n from './i18n'; // initialized i18next instance
+
 import ItemList from './containers/ItemListContainer';
 
 const store = createStore(
@@ -19,9 +22,11 @@ const store = createStore(
 
 const routes = (
   <Router>
-    <App>
-      <Route exact path="/" component={ItemList}/>
-    </App>
+    <I18nextProvider i18n={ i18n }>
+      <App>
+        <Route exact path="/" component={ItemList}/>
+      </App>
+    </I18nextProvider>
   </Router>
 );
 
