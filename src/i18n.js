6aa17f53678195ebf313073c77f135938dae4944
toysroom <info@toysroom.it>
2017-05-26 11:51:28 +0200

    8 intl (#17)

---
+++ src/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+
+i18n
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+
+    resources: {
+      en: {
+        itemList: {
+          empty: 'No items availables'
+        }
+      },
+      it: {
+        itemList: {
+          empty: 'Nessun items disponibile'
+        }
+      }
+    },
+
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it does escape per default to prevent xss!
+    }
+  });
+
+
+export default i18n;
