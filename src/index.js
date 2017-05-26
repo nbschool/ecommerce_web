b290e7eda9cbc27cba80e50d33776cf555088c41
Eugenio Minissale <eugeniominissale@gmail.com>
2017-05-26 10:44:19 +0200

    categories added into App.

--- src/index.js
+++ src/index.js
@@ -11,16 +11,25 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import rootReducer from './modules';
 import App from './components/App';
 import ItemList from './containers/ItemListContainer';
+import CategoryMenu from './components/CategoryMenu';
 
 const store = createStore(
   rootReducer,
   applyMiddleware(thunkMiddleware)
 );
 
+const CATEGORIES = [
+      {name: 'abbigliamento uomo', id: 1},
+      {name: 'abbigliamento donna', id: 2},
+      {name: 'scarpe', id: 3},
+      {name: 'accessori', id: 4},]
+
 const routes = (
   <Router>
     <App>
+      <CategoryMenu listCategories={CATEGORIES}/>
       <Route exact path="/" component={ItemList}/>
+      <Route path="/:category" component={ItemList}/>
     </App>
   </Router>
 );
