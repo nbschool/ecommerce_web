85c4af0415aa7a21c24f96018907affb68f23b13
Francesco Mirabelli <ceskomira90@gmail.com>
2017-05-31 11:52:57 +0200

    translate imported to Item for cart

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { translate } from 'react-i18next';
+
 
 import placehold from './placehold.png';
 import './Item.css';
 
-const Item = ({uuid, name, price, description, pictureUrl}) => (
+const Item = ({uuid, name, price, description, pictureUrl, t}) => (
   <article key={uuid} className="Item">
+
     <div className="card">
       <div className="image">
         <img src={pictureUrl ? pictureUrl : placehold} alt={name} />
@@ -14,8 +17,13 @@ const Item = ({uuid, name, price, description, pictureUrl}) => (
         <div className="name">{name}</div>
         <div className="price">€{price}</div>
         <div className="description block-with-text">{description}</div>
+        <div className="overlay">
+          <button className="addToCart">{t('item:addToCart')}</button>
+          <button className="removeFromCart">{t('removeFromCart')}</button>
+        </div>
       </div>
     </div>
+
   </article>
 );
 
@@ -26,6 +34,7 @@ Item.propTypes = {
   description: PropTypes.string.isRequired,
   pictureUrl: PropTypes.string,
   category: PropTypes.string.isRequired,
+  t: PropTypes.func.isRequired
 };
 
-export default Item;
+export default translate('item')(Item);
