c69a727100deaa2d62bc467f532f5cfddcbd7475
German Lugo <bitnibs@gmail.com>
2017-06-06 12:00:41 +0200

    Reuse item as a constant.

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,41 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 
 import placehold from './placehold.png';
 import './Item.css';
 
-class Item extends Component {
-  render() {
-    const {t} = this.props;
-    let emojiStock, textStock;
-    if (this.props.tagStock) {
-      emojiStock = '✅';
-      textStock = t('item:in_stock');
-    } else {
-      emojiStock = '❌';
-      textStock = t('item:out_stock');
-    }
-    return (
-      <article key={this.props.uuid} className="Item">
-        <div className="card">
-          <div className="image">
-            <img src={this.props.pictureUrl ? this.props.pictureUrl : placehold}
-             alt={this.props.name} />
-          </div>
-          <div className="info">
-            <div className="name">{this.props.name}</div>
-            <div className="price">€{this.props.price}</div>
-            <div className="description block-with-text">{this.props.description}</div>
-          </div>
-          <div className="in_Stock">
-            {emojiStock}<p>{textStock}</p>
-          </div>
-        </div>
-      </article>
-    );
+const Item = (props) => {
+  const {t, tagStock} = props;
+  let emojiStock, textStock;
+  if (tagStock) {
+    emojiStock = '✅';
+    textStock = t('item:in_stock');
+  } else {
+    emojiStock = '❌';
+    textStock = t('item:out_stock');
   }
-}
+
+  return (
+    <article key={props.uuid} className="Item">
+      <div className="card">
+        <div className="image">
+          <img src={props.pictureUrl ? props.pictureUrl : placehold}
+           alt={props.name} />
+        </div>
+        <div className="info">
+          <div className="name">{props.name}</div>
+          <div className="price">€{props.price}</div>
+          <div className="description block-with-text">{props.description}</div>
+        </div>
+        <div className="in_Stock">
+          {emojiStock}<p>{textStock}</p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
 
 Item.propTypes = {
   uuid: PropTypes.string.isRequired,
